216a79ff2636fada333631dcb90bddf2f5784274
datainsightagent <datainsight.agent@gmail.com>
2026-07-16 11:54:55 +0530

    Add files via upload

--- src/routes/widget.routes.js
+++ src/routes/widget.routes.js
@@ -3,12 +3,14 @@ import {
   deleteWidgetHandler,
   getWidgetDataHandler,
   getWidgetHandler,
+  patchWidgetHandler,
 } from "../controllers/dashboard.controller.js";
 
 const router = express.Router();
 
 router.get("/:widget_id/data", getWidgetDataHandler);
 router.get("/:widget_id", getWidgetHandler);
+router.patch("/:widget_id", patchWidgetHandler);
 router.delete("/:widget_id", deleteWidgetHandler);
 
 export default router;
